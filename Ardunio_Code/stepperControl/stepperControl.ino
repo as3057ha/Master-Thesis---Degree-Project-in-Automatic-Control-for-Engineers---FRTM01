51246440c04065c0272097628529dbbbdb6c6f6b
Aslan <as3057ha-s@student.lu.se>
2024-02-28 09:16:44 +0100

    Changing RPM working, modify to be synchronized with the linear movement of the TCP

--- Ardunio_Code/stepperControl/stepperControl.ino
+++ Ardunio_Code/stepperControl/stepperControl.ino
@@ -5,37 +5,25 @@ const int DIR_PIN = 8; // DIR- connected to pin 8
 const int PUL_PIN = 9; // PUL- connected to pin 9
 
 // Define control variables
-const int STEPS_PER_REVOLUTION = 3200; // Set this to the number of pulses per revolution as per DIP switch settings
-const double SAMPLE_TIME = 0.1 * 1000; // In miliseconds
+const double STEPS_PER_REVOLUTION = 800; // Set this to the number of pulses per revolution as per DIP switch settings
+const double SAMPLE_TIME = 500; // In miliseconds
 
 // Stepper definition
 Stepper JMC(STEPS_PER_REVOLUTION,PUL_PIN,DIR_PIN);
 
 // Define physical variables
-const double NUMBER_TEETH_A = 32;
-const double NUMBER_TEETH_B = 60;
-const double FEEDING_RADIUS = 0.05; // In meters 
+const float NUMBER_TEETH_A = 32;
+const float NUMBER_TEETH_B = 60;
+const float FEEDING_RADIUS = (30.0 - 1.427)*0.001; // In meters 
 
 // Velocity
-double vTcp;
-double rpm;
+float vTcp;
+float rpm;
 int steps;
 
 // Mock data
-double vectorData[] = {
-    0.0005923316160294426, -0.00010162865124387814, -6.350145888285325e-05, 0.016567409702998318, 0.02031301213100328,
-    0.039191360139298095, 0.04485468382104221, 0.061892197622671084, 0.08104223845887772, 0.0960026794041281,
-    0.1301056722696201, 0.14603005962256216, 0.1803643151348643, 0.2077796537233585, 0.24086499817887638,
-    0.27752744682023295, 0.317042049311117, 0.3655560531800093, 0.40372121756680035, 0.4466164264865411,
-    0.49960510503603506, 0.5008395768285745, 0.49868352823118817, 0.4989555376133149, 0.5095487986934275,
-    0.48928230356882774, 0.49288713661499356, 0.5126332756436758, 0.49959272859014836, 0.5077766861150973,
-    0.5025090994028439, 0.5079995610991677, 0.49921981924918996, 0.5022299838896609, 0.4914433883473273,
-    0.49242010595099556, 0.47635897765675794, 0.46374710927750573, 0.46245896754703647, 0.4479422837332736,
-    0.42623894200566825, 0.4153795724045813, 0.40894964570689013, 0.37753454843945905, 0.361723385197715,
-    0.33443248199062087, 0.30724047922689757, 0.2879479991368391, 0.24875197098947824, 0.22903976606824003,
-    0.18856741640727212, 0.1713098900731542, 0.12913118404039092, 0.10551717293616283, 0.06369625631762069,
-    0.015065912578921915, 0.005795811647082465, -0.006424028443371094, -0.0006031530911661132, 0.003445980145674999,
-    0.0026981447269134673, 0.006623734398884928
+float vectorData[] = {
+    0.009359928372335902,0.03638185375370186,0.09325720705333092,0.13927239806346536,0.19323586187029457,0.2620708178503811,0.29315063982769296,0.3488900271237711,0.38079455255917793,0.445191544375395,0.5039192239572305,0.4988673183973963,0.510765491008229,0.5017196892340765,0.514772276314553,0.498938678758812,0.5107059696198987,0.4949068549209342,0.5065431825475957,0.5115436237837065,0.4961576336201521,0.49583723970585114,0.0024730499707425885
 };
 int currentIndex = 0;
 
@@ -43,38 +31,49 @@ int totalElements = sizeof(vectorData) / sizeof(vectorData[0]);
 
 
 void setup() {
+  //Serial communication for debugging
+  Serial.begin(9600); 
   // Set DIR_PIN and PUL_PIN as outputs
   pinMode(DIR_PIN, OUTPUT);
   pinMode(PUL_PIN, OUTPUT);
   
   // Set DIR_PIN low
-  digitalWrite(DIR_PIN, LOW);
+  digitalWrite(DIR_PIN, HIGH);
+  Serial.println("Setup Complete");
 }
 
 void loop() {
+  Serial.println("-----------------------------");
+  Serial.println("Current index: " + (String)currentIndex);
 
   // Get the current time
   unsigned long t0 = millis();
   // Read robot velocity
   vTcp = readVTcp();
+  Serial.println("vTcp: " + (String)vTcp);
   // Calculate corrisponding rpm
   rpm = calculateRPM(vTcp);
+  Serial.println("rpm: " + (String)rpm);
   // Set the motor speed 
   JMC.setSpeed(rpm);
   // Calculate the number of steps needed to maintain the sample time
   steps = calculateNbrSteps(rpm);
   // Step the motor
+  Serial.println("Steps: " + (String)steps);
   JMC.step(steps);
   // Get the current time
   unsigned long t1 = millis();
   // Calculate the time passed in this loop iteration
   unsigned long tPassed = t1 - t0;
+  Serial.println("Loop time in ml" + (String)tPassed);
 
-  // Check if the time passed is less than the desired sample time
-  if (tPassed < SAMPLE_TIME){
-    // If so, delay for the remaining time to meet the sample time
-    delay(SAMPLE_TIME- tPassed);
-  }
+ // Serial.println("Steps per s" + (String)((1/60)*steps*rpm));
+
+  // // Check if the time passed is less than the desired sample time
+  // if (tPassed < SAMPLE_TIME){
+  //   // If so, delay for the remaining time to meet the sample time
+  //   delay(SAMPLE_TIME- tPassed);
+  // }
   
 }
 
@@ -88,15 +87,34 @@ double readVTcp() {
     }
 
     // Return the current value and update the index for the next call
-    double currentValue = vectorData[currentIndex];
+    float currentValue = vectorData[currentIndex];
     currentIndex++;
     return currentValue;
 }
 
-double calculateRPM(double vTcp) {return vTcp*60*NUMBER_TEETH_B/(FEEDING_RADIUS*2*PI*NUMBER_TEETH_A);}
+double calculateRPM(double vTcp) {
+  return vTcp*60*NUMBER_TEETH_B/(FEEDING_RADIUS*2*PI*NUMBER_TEETH_A);}
 
 int calculateNbrSteps(double rpm){
-  int nbrSteps = (int)(SAMPLE_TIME*STEPS_PER_REVOLUTION*rpm/60);
+
+  // Debugging prints for each variable
+  Serial.print("SAMPLE_TIME: ");
+  Serial.println(SAMPLE_TIME);
+  Serial.print("STEPS_PER_REVOLUTION: ");
+  Serial.println(STEPS_PER_REVOLUTION);
+  Serial.print("rpm: ");
+  Serial.println(rpm);
+
+  // Now perform the calculation
+  float rpmOverTime = rpm / 60.0; // RPM to RPS (Revolutions per Second)
+  float stepsPerSecond = STEPS_PER_REVOLUTION * rpmOverTime;
+  float stepsPerMillisecond = stepsPerSecond / 1000.0; // Convert seconds to milliseconds
+  float dnbrSteps = SAMPLE_TIME * stepsPerMillisecond;
+
+  // Print the result of the calculation
+  Serial.print("dnbrSteps (before casting): ");
+  Serial.println(dnbrSteps);
+  int nbrSteps = (int)dnbrSteps;
   return nbrSteps;
 }
 
--- ros2_ws/src/build/ur_gazebo/colcon_command_prefix_build.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/albin/Master-Thesis---Degree-Project-in-Automatic-Control-for-Engineers---FRTM01/ros2_ws/src/install/ur_description/share/ur_description/package.sh"
